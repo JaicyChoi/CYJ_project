436eca645ee158e47cf2df3cbe6eb8be4c106b8d
JaicyChoi <jaicychoi@gmail.com>
2020-06-03 18:09:19 +0900

    at green modified

--- js/data.js
+++ js/data.js
@@ -1,4 +1,17 @@
 let DATA = {
+    Main_Visual: [
+        '오늘도 수고했어. 맥주 한 잔 어때? 취향 저격 맥주 추천 받기 Go',
+        '지금 Craft Zone 방문하면 페일 에일 한 잔이 무료',
+        'Upright Special 스페셜 시음회를 개최합니다',
+        '2019 신촌 맥주 축제',
+        '부엉이 키친 방문 후기 남기면 안주 1종이 무료'
+    ],
+    Magazine: [
+        { 
+            alt: '매거진: 라거에 대하여',
+            text: '발효방식에따라 크게 세 가지로 구분하는 맥주의 갈래 중 하나. 에일(상면발효), 람빅(자연발효), 라거(하면발효)이다. 일반적으로 섭씨 7~13 도에서 발효되며, 발효기간은 약 2~4 주. 이후오랜 숙성기간을 거쳐 에일과는 다르게 맑아지는게 특징이다. 7~13 도는 늦가을 ~ 초봄이 아니면 실온에서 발효가 어려워 중세~근대 유럽에서는 동굴, 지하실등에 저장해서 발효했다고 한다…'
+        }
+    ],
     New_Beer_Arrivals: [
         {
             abv: '4.7%',
@@ -6,15 +19,55 @@ let DATA = {
             country_alt: '미국',
             beer: 'Mamma_Mia_Pizza_Beer',
             beer_alt: '맘마 미아 피자 비어',
-            title: '맘마 미아 피자 비어'
+            title: '맘마! 미아 피자 비어'
         },
         {
-            abv: '4.7%',
+            abv: '5.0%',
             country: 'france',
             country_alt: '프랑스',
             beer: 'Weihenstephan_Original',
             beer_alt: '크로넨버그 1164 블랑',
             title: '크로넨버그 1164 블랑'
+        },
+        {
+            abv: '4.8%',
+            country: 'germany',
+            country_alt: '독일',
+            beer: 'kostritzer_Schwarzbier',
+            beer_alt: '쾨스트리처 슈바르츠비어',
+            title: '쾨스트리처 슈바르츠비어'
+        },
+        {
+            abv: '4.8%',
+            country: 'germany',
+            country_alt: '독일',
+            beer: 'Bitburger_Premium_Pils',
+            beer_alt: '비트부르거 프리미엄 필스',
+            title: '비트부르거 프리미엄 필스'
+        },
+        {
+            abv: '5.1%',
+            country: 'germany',
+            country_alt: '독일',
+            beer: 'Weihenstephan_Original',
+            beer_alt: '바이헨슈테파너 오리지날',
+            title: '바이헨슈테파너 오리지날'
+        },
+        {
+            abv: '4.8%',
+            country: 'spain',
+            country_alt: '스페인',
+            beer: 'Estrella_Damm_Inedit',
+            beer_alt: '에스트레야 담 이네딧',
+            title: '에스트레야 담 이네딧'
+        },
+        {
+            abv: '4.2%',
+            country: 'Ireland',
+            country_alt: '아일랜드',
+            beer: 'Guinness Draught',
+            beer_alt: '기네스 드래프트',
+            title: '기네스 드래프트'
         }
     ],
     Beer_of_the_Month: [
@@ -22,11 +75,11 @@ let DATA = {
             beer_img: 'Gaffels_SonnenHopfen',
             beer_alt: '가펠 소넨호펜',
             title: 'Gafeels SonnonHofen <span class="beer_description_title_kor"> 가펠 소넨호펜</span>',
-            country: 'Germany',
+            country: 'germany',
             country_alt: '독일',
             abv: 'ABV <span class="bold">4.7%</span>',
-            style: '맥주 스타일 <span class="bold">test</span>',
-            flavor: '맛/향 <span class="bold">test</span>',
+            style: '맥주 스타일 <span class="bold">쾰시</span>',
+            flavor: '맛/향 <span class="bold">소나무와 레몬향</span>',
             text: '숲의 기운이 가득 느껴치는 향으로, 과일같은 달콤한 기운이 감도는 홉 향이 특징. 감귤처럼 달콤하면서도 레몬처럼 새콤하다. 부드럽고 마시기 좋은 맥주.'
         },
         {
@@ -35,10 +88,10 @@ let DATA = {
             title: 'Efes Pilsener <span class="beer_description_title_kor"> 에페스 필스너</span>',
             country: 'terkey',
             country_alt: '터키',
-            abv: 'ABV <span class="bold">4.7%</span>',
-            style: '맥주 스타일 <span class="bold">test1</span>',
-            flavor: '맛/향 <span class="bold">test1</span>',
-            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec dictum libero, hendrerit viverra enim. Ut a eros suscipit, condimentum est molestie, lobortis dolor.'
+            abv: 'ABV <span class="bold">5.0%</span>',
+            style: '맥주 스타일 <span class="bold">필스너</span>',
+            flavor: '맛/향 <span class="bold">강한 홉의 향, 깔끔한 맛</span>',
+            text: '청량감이 좋고 상쾌하며 약간 시큼한 맛과 쌉쌀한 맛이 난다. 강한 홉의 향과 함께 구수하고 달달한 향이 올라온다. 개성이 확실한 맥주로 가벼운 맥주를 좋아하는 사람에게 추천.'
         },
         {
             beer_img: 'Jacobins_Rouge',
@@ -46,10 +99,10 @@ let DATA = {
             title: 'Jacobins Rouge <span class="beer_description_title_kor"> 자코뱅 루즈</span>',
             country: 'belgie',
             country_alt: '벨기에',
-            abv: 'ABV <span class="bold">4.7%</span>',
-            style: '맥주 스타일 <span class="bold">test2</span>',
-            flavor: '맛/향 <span class="bold">test2</span>',
-            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec dictum libero, hendrerit viverra enim. Ut a eros suscipit, condimentum est molestie, lobortis dolor.'
+            abv: 'ABV <span class="bold">5.5%</span>',
+            style: '맥주 스타일 <span class="bold">플랜더스 레드</span>',
+            flavor: '맛/향 <span class="bold">식초와 체리의 맛, 오크 나무 향</span>',
+            text: '강한 식초의 맛과 체리, 사과 등의 과일 맛을 느낄 수 있으며 단맛은 거의 느껴지지 않는 깔끔한고 개운한(Dry)한 맥주. 탄산의 기운이 강한 편으로 입안으로 퍼짐이 확연히 느껴진다.'
         },
         {
             beer_img: 'Hoppy_Daze_IPA',
@@ -57,10 +110,39 @@ let DATA = {
             title: 'Hoppy Daze IPA <span class="beer_description_title_kor"> 홉피 데이즈 IPA</span>',
             country: 'usa',
             country_alt: '미국',
-            abv: 'ABV <span class="bold">4.7%</span>',
-            style: '맥주 스타일 <span class="bold">test3</span>',
-            flavor: '맛/향 <span class="bold">test3</span>',
-            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec dictum libero, hendrerit viverra enim. Ut a eros suscipit, condimentum est molestie, lobortis dolor.'
+            abv: 'ABV <span class="bold">7.5%</span>',
+            style: '맥주 스타일 <span class="bold">벨지안 IPA</span>',
+            flavor: '맛/향 <span class="bold">소나무와 감귤향</span>',
+            text: '소나무와 감귤, 레몬의 향과 함께 홉의 쌉쌀함이 효모의 알싸함과 합쳐져 코를 자극한다. 탄산은 적당한 편이며 꿀이나 시럽을 연상시키는 단만이 난다.'
         }
+    ],
+    Weekly_Best_Review: [
+        {
+            preview_alt: '벨스 스페셜 더블 크림 스타우트 리뷰',
+            view_count: 7469,
+            recommend_count: 128,
+            preview_title: '벨스 스페셜 더블 크림 스타우트',
+            text: '깜장 맥주 마시기 좋은 계절이라 저는 요즘 아주 신이 납니다. 벨스의 여러 스페셜 라인업 중에서 오늘 마실 \'Special Double Cream Stout\'는 8월 부터 이듬해 2월 까지 마실 수 있는 스타우트입니다. 깜장 맥주 마시기 좋은 계절이라 저는 요즘 아주...'
+        },
+        {
+            preview_alt: '몬테크리스토 샌드위치 레시피',
+            view_count: 6213,
+            recommend_count: 157,
+            preview_title: '몬테크리스토 샌드위치',
+            text: '샌드위치 2탄 어쩌면 이 샌드위치는 조금 더복잡한 과정이지만 비슷해서 올려드려요.. 왜 이름이 몬테크리스토이냐구요? 두얼굴의 샌드위치거든요 샌드위치지만 샌드위치가 아닌것처럼 어쩜 고로께인것 처럼 분장을 해서 그렇게 이름이 붙여진것..'
+        },
+        {
+            preview_alt: 'THE 8LOW 소개',
+            view_count: 4235,
+            recommend_count: 71,
+            preview_title: 'THE 8LOW',
+            text: '안녕하세요 !! 수유동에서 가장 맛있는 수제맥주집 \'THE 8LOW\'입니다. 수유동에 수제맥주집이 흔치 않은데 사일구 카페거리에 위치한 분위기 좋은 브런치카페&펍 이구요!! 요즘 대세는 피맥! 파맥! 직접 소스도 만들어 굽는 수제피자와 다양..'
+        }
+    ],
+    View_Latest_Event: [
+        '이달의 맥주 찰떡궁합 레시피',
+        '이달의 맥주 BEST 10',
+        '이달의 맥주 축제',
+        '무료 샘플 신청하기'
     ]
 };
--- js/main_contents.js
+++ js/main_contents.js
@@ -1,52 +1,110 @@
-let getNodeindex = elm => [...elm.parentNode.children].indexOf(elm);
-
 //New beer arrivals
-const new_beer_arrivals_abv = document.querySelector('#new_beer_arrivals_abv');
-const new_beer_arrivals_country = document.querySelector('#new_beer_arrivals_country');
-const new_beer_arrivals_beer = document.querySelector('#new_beer_arrivals_beer');
-const new_beer_arrivals_title = document.querySelector('#new_beer_arrivals_title');
-const new_beer_list = document.querySelectorAll('#new_beer_list li');
-// const new_beer_list = document.querySelectorAll('.beer_list');
+const new_beer_preview = document.querySelector('#new_beer_preview');
+const new_beer_list = document.querySelector('#new_beer_list');
 const New_Beer_Arrivals = DATA.New_Beer_Arrivals;
 
-new_beer_list.forEach(new_beer_list =>
+for( let i = 0 ; i < New_Beer_Arrivals.length - 1; i++ ){
+    let new_beer_li = document.createElement('li');
+    new_beer_li.innerHTML = '<a href="#">' + New_Beer_Arrivals[i].title + '</a>';
+    new_beer_list.appendChild(new_beer_li);
+}
+
+const new_beer_list_all = document.querySelectorAll('#new_beer_list li');
+let new_beer_arrivals_abv = document.createElement('span');
+let new_beer_arrivals_country = document.createElement('img');
+let new_beer_arrivals_beer = document.createElement('img');
+let new_beer_arrivals_title = document.createElement('p');
+
+new_beer_arrivals_abv.id = 'new_beer_arrivals_abv';
+new_beer_arrivals_country.id = 'new_beer_arrivals_country';
+new_beer_arrivals_beer.id = 'new_beer_arrivals_beer';
+new_beer_arrivals_title.id = 'new_beer_arrivals_title';
+
+new_beer_preview.appendChild(new_beer_arrivals_abv);
+new_beer_preview.appendChild(new_beer_arrivals_country);
+new_beer_preview.appendChild(new_beer_arrivals_beer);
+new_beer_preview.appendChild(new_beer_arrivals_title);
+
+(function New_Beer_Arrivals_init(){
+    new_beer_arrivals_abv.innerHTML = New_Beer_Arrivals[0].abv;
+    new_beer_arrivals_country.src = 'images/country/' + New_Beer_Arrivals[0].country + '.png';
+    new_beer_arrivals_country.alt = New_Beer_Arrivals[0].country_alt;
+    new_beer_arrivals_beer.src = 'images/beer/' + New_Beer_Arrivals[0].beer + '.png';
+    new_beer_arrivals_beer.alt = New_Beer_Arrivals[0].beer_alt;
+    new_beer_arrivals_title.innerHTML = New_Beer_Arrivals[0].title;
+}());
+
+new_beer_list_all.forEach(new_beer_list =>
     new_beer_list.addEventListener('mouseenter', show_beer_preview));
 
 function show_beer_preview(){
-    console.log(getNodeindex(this));
     new_beer_arrivals_abv.innerHTML = New_Beer_Arrivals[getNodeindex(this)].abv;
-    new_beer_arrivals_country.src = 'images/country/' + New_Beer_Arrivals[getNodeindex(this)].country + '/.png';
+    new_beer_arrivals_country.src = 'images/country/' + New_Beer_Arrivals[getNodeindex(this)].country + '.png';
     new_beer_arrivals_country.alt = New_Beer_Arrivals[getNodeindex(this)].country_alt;
     new_beer_arrivals_beer.src = 'images/beer/' + New_Beer_Arrivals[getNodeindex(this)].beer + '.png';
     new_beer_arrivals_beer.alt = New_Beer_Arrivals[getNodeindex(this)].beer_alt;
     new_beer_arrivals_title.innerHTML = New_Beer_Arrivals[getNodeindex(this)].title;
 }
 
+//Magazine
+const magazine_preview = document.querySelector('#magazine_preview');
+const magazine_text = document.querySelector('#magazine_text');
+const Magazine = DATA.Magazine[0];
+
+magazine_preview.alt = Magazine.alt;
+magazine_text.innerHTML = Magazine.text;
+
 //Beer of the month
 const beer_preview = document.querySelector('#beer_preview');
-const beer_description_title = document.querySelector('#beer_description_title');
-const beer_description_country = document.querySelector('#beer_description_country');
-const beer_description_abv = document.querySelector('#beer_description_abv');
-const beer_description_style = document.querySelector('#beer_description_style');
-const beer_description_text = document.querySelector('#beer_description_text');
-const selected_beer = document.querySelectorAll('.selected_beer');
+const beer_description = document.querySelector('#beer_description');
+const beer_select = document.querySelector('#beer_select');
 const Beer_of_the_Month = DATA.Beer_of_the_Month;
 
-Beer_of_the_Month_init();
+for( let i = 0 ; i <= Beer_of_the_Month.length - 1 ; i++ ){
+    let beer_select_li = document.createElement('li');
+    let beer_select_img = document.createElement('img');
+    
+    beer_select_img.src = 'images/beer/' + Beer_of_the_Month[i].beer_img + '.png';
+    beer_select_img.alt = Beer_of_the_Month[i].beer_alt;
+    beer_select_li.appendChild(beer_select_img);
+    beer_select.appendChild(beer_select_li);
+}
 
-function Beer_of_the_Month_init(){
+const selected_beer = document.querySelectorAll('#beer_select li');
+let beer_description_title = document.createElement('p');
+let beer_description_country = document.createElement('img');
+let beer_description_abv = document.createElement('p');
+let beer_description_style = document.createElement('p');
+let beer_description_flavor = document.createElement('p');
+let beer_description_text = document.createElement('p');
+
+beer_description_title.id = 'beer_description_title';
+beer_description_country.id = 'beer_description_country';
+beer_description_abv.id = 'beer_description_abv';
+beer_description_style.id = 'beer_description_style';
+beer_description_flavor.id = 'beer_description_flavor';
+beer_description_text.id = 'beer_description_text';
+
+beer_description.appendChild(beer_description_title);
+beer_description.appendChild(beer_description_country);
+beer_description.appendChild(beer_description_abv);
+beer_description.appendChild(beer_description_style);
+beer_description.appendChild(beer_description_flavor);
+beer_description.appendChild(beer_description_text);
+
+(function Beer_of_the_Month_init(){
     selected_beer[0].classList.add('mouseover');
 
     beer_preview.src = 'images/beer/' + Beer_of_the_Month[0].beer_img + '.png';
     beer_preview.alt = Beer_of_the_Month[0].beer_alt;
     beer_description_title.innerHTML = Beer_of_the_Month[0].title;
-    beer_description_country.src = 'images/contury/' + Beer_of_the_Month[0].country + '.png';
+    beer_description_country.src = 'images/country/' + Beer_of_the_Month[0].country + '.png';
     beer_description_country.alt = Beer_of_the_Month[0].country_alt;
     beer_description_abv.innerHTML = Beer_of_the_Month[0].abv;
     beer_description_style.innerHTML = Beer_of_the_Month[0].style;
     beer_description_flavor.innerHTML = Beer_of_the_Month[0].flavor;
     beer_description_text.innerHTML = Beer_of_the_Month[0].text;
-}
+}());
 
 selected_beer.forEach(selected_beer => 
     selected_beer.addEventListener('mouseenter', show_description));
@@ -59,10 +117,89 @@ function show_description(){
     beer_preview.src = 'images/beer/' + Beer_of_the_Month[getNodeindex(this)].beer_img + '.png';
     beer_preview.alt = Beer_of_the_Month[getNodeindex(this)].beer_alt;
     beer_description_title.innerHTML = Beer_of_the_Month[getNodeindex(this)].title;
-    beer_description_country.src = 'images/contury/' + Beer_of_the_Month[getNodeindex(this)].country + '.png';
+    beer_description_country.src = 'images/country/' + Beer_of_the_Month[getNodeindex(this)].country + '.png';
     beer_description_country.alt = Beer_of_the_Month[getNodeindex(this)].country_alt;
     beer_description_abv.innerHTML = Beer_of_the_Month[getNodeindex(this)].abv;
     beer_description_style.innerHTML = Beer_of_the_Month[getNodeindex(this)].style;
     beer_description_flavor.innerHTML = Beer_of_the_Month[getNodeindex(this)].flavor;
     beer_description_text.innerHTML = Beer_of_the_Month[getNodeindex(this)].text;
+}
+
+//Weekly best reviewer
+const best_review_img = document.querySelector('#best_review_img');
+const weekly_best_reviewer = document.querySelector('#weekly_best_reviewer');
+const weekly_best_recipe = document.querySelector('#weekly_best_recipe');
+const hottest_pub_store = document.querySelector('#hottest_pub_store');
+const Weekly_Best_Review = DATA.Weekly_Best_Review;
+const preview_box = document.querySelectorAll('.preview_box');
+
+let user_count = document.createElement('p');
+let view_count = document.createElement('span')
+let recommend_count = document.createElement('span');
+let preview_title = document.createElement('span');
+
+user_count.classList.add('user_count');
+view_count.classList.add('view_count');
+recommend_count.classList.add('recommend_count');
+preview_title.classList.add('preview_title');
+
+for( let i = 0 ; i <= Weekly_Best_Review.length - 1 ; i++ ){
+    let review_text = document.createElement('p');
+    review_text.classList.add('review_text');
+    review_text.innerHTML = Weekly_Best_Review[i].text;
+    preview_box[i].parentNode.parentNode.appendChild(review_text);
+}
+
+user_count.appendChild(view_count);
+user_count.appendChild(recommend_count);
+user_count.appendChild(preview_title);
+
+preview_box.forEach(preview_box => 
+    preview_box.parentNode.parentNode.addEventListener('mouseenter', show_info));
+preview_box.forEach(preview_box => 
+    preview_box.parentNode.parentNode.addEventListener('mouseleave', hide_info));
+
+function show_info(){
+    this.childNodes[1].childNodes[3].appendChild(user_count);
+    this.childNodes[1].childNodes[3].lastChild.style.cssText = 'display: block';
+
+    let find_section = this.id;
+    let section_num;
+
+    if( find_section === weekly_best_reviewer.id.toString() ) section_num = 0;
+    else if( find_section === weekly_best_recipe.id.toString() ) section_num = 1;
+    else section_num = 2;
+  
+    best_review_img.alt = Weekly_Best_Review[section_num].preview_alt;
+    view_count.innerHTML = Weekly_Best_Review[section_num].view_count;
+    recommend_count.innerHTML = Weekly_Best_Review[section_num].recommend_count;
+    preview_title.innerHTML = Weekly_Best_Review[section_num].preview_title;
+
+    this.lastChild.style.cssText = 'opacity: 1; text-decoration: underline;';
+}
+
+function hide_info(){
+    this.childNodes[1].childNodes[3].lastChild.style.cssText = 'display:none;';
+    this.lastChild.style.cssText = 'opacity: 0.8; text-decoration: none;';
+}
+
+//view_latest_event
+const view_latest_event_box = document.querySelector('#view_latest_event_box');
+const View_Latest_Event = DATA.View_Latest_Event;
+
+for( let i = View_Latest_Event.length - 1 ; i >= 0 ; i-- ){
+    let a = document.createElement('a');
+    let event_preview_box = document.createElement('div');
+    let event_preview = document.createElement('img');
+
+    a.href = '#';
+    event_preview_box.classList.add('event_preview_box');
+    event_preview.classList.add('event_preview');
+
+    event_preview.src= 'images/event' + ( i + 1 ) + '.png';
+    event_preview.alt = View_Latest_Event[i];
+    event_preview_box.appendChild(event_preview);
+    a.appendChild(event_preview_box);
+
+    view_latest_event_box.insertBefore(a, view_latest_event_box.firstChild);
 }
